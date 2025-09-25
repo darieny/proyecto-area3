6125122059023371bb2ba80e00e086b55e21cc39
darieny <darienycarpio@gmail.com>
2025-09-25 13:08:38 -0600

    Solucion

--- backend/api/[...all].js
+++ backend/api/[...all].js
@@ -1,7 +1,10 @@
-// backend/api/[...all].js
 import app from '../src/app.js';
 
 export default function handler(req, res) {
+  if (req.url && req.url.startsWith('/api')) {
+    req.url = req.url.replace(/^\/api/, '') || '/';
+  }
   return app(req, res);
 }
+
 
