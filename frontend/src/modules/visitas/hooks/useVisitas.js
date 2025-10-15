94619b615567aed8c64729d81f455cf2766b8d3e
darieny <darienycarpio@gmail.com>
2025-10-14 18:24:28 -0600

    se importó libreria useCallback

--- frontend/src/modules/visitas/hooks/useVisitas.js
+++ frontend/src/modules/visitas/hooks/useVisitas.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { visitasApi } from "../../../services/visitas.api.js";
 
 export function useVisitas(initial = {}) {
